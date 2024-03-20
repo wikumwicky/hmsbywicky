118507fb08fef85187718eddbd0d71dfc60d37c0
wikumwicky <wikumwick@gmail.com>
2024-03-20 20:11:28 +0530

    intial commit

---
+++ src/test/java/wikum/testmovie/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package wikum.testmovie.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
